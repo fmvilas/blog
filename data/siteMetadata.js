77fd2f31ad4174619220dcc8dee4d6efa5497146
Fran Mendez <fmvilas@gmail.com>
2022-01-25 20:52:38 +0100

    Initial commit

---
+++ data/siteMetadata.js
@@ -0,0 +1,24 @@
+const siteMetadata = {
+  title: 'Fran Mendez\'s blog',
+  author: 'Fran Mendez',
+  headerTitle: 'Fran Mendez\'s blog',
+  description: 'This is the blog of Fran Mendez.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://www.franmendez.blog',
+  siteRepo: 'https://github.com/fmvilas/blog',
+  siteLogo: '',
+  image: '/static/images/avatar.jpg',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'fmvilas@gmail.com',
+  github: 'https://github.com/fmvilas',
+  twitter: 'https://twitter.com/fmvilas',
+  youtube: 'https://youtube.com/fmvilas',
+  linkedin: 'https://www.linkedin.com/in/fmvilas',
+  locale: 'en-US',
+  analytics: {
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
+}
+
+module.exports = siteMetadata
