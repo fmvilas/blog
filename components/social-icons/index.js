1b15af1a9d44f989bf27571a7e29afccb70b4181
Fran Mendez <fmvilas@gmail.com>
2022-11-20 06:14:02 -0800

    Add rel=me to my profile links

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -28,7 +28,7 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     <a
       className="text-sm text-gray-500 transition hover:text-gray-600"
       target="_blank"
-      rel="noopener noreferrer"
+      rel="me noopener noreferrer"
       href={href}
     >
       <span className="sr-only">{kind}</span>
