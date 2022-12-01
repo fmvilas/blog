09375029d60ff801714f5dc65b8e210fbf58eaa4
Fran Mendez <fmvilas@gmail.com>
2022-12-01 12:47:26 +0100

    Add book and shapeup to menu

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,8 @@
 const headerNavLinks = [
+  { href: 'https://book-eda.franmendez.blog/', title: 'Book' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: 'https://shapeup.franmendez.blog/', title: 'Dashboard' },
 ]
 
 export default headerNavLinks
