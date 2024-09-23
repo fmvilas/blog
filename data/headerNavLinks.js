eaf3be172230d257b983db0cddcd1868a45c9255
fmvilas <fmvilas@gmail.com>
2024-09-23 21:14:00 +0200

    Remove dashboard from header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,6 @@ const headerNavLinks = [
   { href: 'https://book-eda.fmvilas.me/', title: 'Book' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: 'https://shapeup.fmvilas.me/', title: 'Dashboard' },
 ]
 
 export default headerNavLinks
