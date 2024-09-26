28468f277a96eb31db93ec642e08555464eb97f1
fmvilas <fmvilas@gmail.com>
2024-09-26 16:59:35 +0200

    Stop pointing to tol.show

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,9 @@
 const headerNavLinks = [
-  { href: 'https://tol.show', title: 'Thinking Out Loud', target: '_blank' },
+  {
+    href: 'https://www.youtube.com/playlist?list=PLbi1gRlP7pigPxRRylHGCvpdppYLmSKfJ',
+    title: 'Thinking Out Loud',
+    target: '_blank',
+  },
   { href: 'https://book-eda.fmvilas.me/', title: 'Book' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
