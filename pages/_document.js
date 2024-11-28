74459961c7563452aa3a192333f94ae3e4b6bb05
fmvilas <fmvilas@gmail.com>
2024-11-28 16:38:44 +0100

    Add opengraph stuff

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,45 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+const URL = 'https://www.fmvilas.me'
+const TITLE = 'Fran Méndez\'s Mailing List'
+const DESCRIPTION = 'Get an opinion article every Sunday about engineering, design, and life. I\'m Fran Méndez, creator of AsyncAPI, the industry standard for describing asynchronous APIs. Put your email below and I\'ll make sure you always learn something new every week.'
+const IMAGE = '/static/images/opengraph.png'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="scroll-smooth min-w-full w-screen">
         <Head>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:url" content={URL} />
+          <meta property="og:image" content={IMAGE} />
+          <meta property="og:description" content={DESCRIPTION} />
+
+          {/* <!-- For Google --> */}
+          <meta name="description" content={DESCRIPTION} />
+          <meta name="keywords" content="newsletter, opinion, engineering, design, life, asyncapi, api, kafka, mqtt, amqp, rabbitmq, pubsub, messaging, event-driven, eventcatalog" />
+          <meta name="author" content="Fran Méndez" />
+          <meta name="copyright" content="Fran Méndez" />
+          <meta name="application-name" content={TITLE} />
+
+          {/* <!-- For Facebook --> */} 
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:type" content="article" />
+          <meta property="og:image" content={IMAGE} />
+          <meta property="og:url" content={URL} />
+          <meta property="og:description" content={DESCRIPTION} />
+
+          {/*   <!-- For Twitter --> */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content={TITLE} />
+          <meta name="twitter:description" content={DESCRIPTION} />
+          <meta name="twitter:image" content={IMAGE} />
+
           <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <meta name="apple-mobile-web-app-title" content="Fran Méndez's Mailing List" />
+          <meta name="apple-mobile-web-app-title" content={TITLE} />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#ffc103" />
           <meta name="theme-color" content="#ffc103" />
