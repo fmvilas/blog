a167c4572a61b6084240a94dd99f22ef86eb6ee8
fmvilas <fmvilas@gmail.com>
2025-09-16 12:31:30 +0200

    Add Commune landing page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Commune from "./pages/Commune";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/commune" element={<Commune />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
