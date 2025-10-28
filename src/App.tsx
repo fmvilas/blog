90abaf653f974c07b1eb72755b0f35c123c68156
fmvilas <fmvilas@gmail.com>
2025-10-28 08:05:14 +0100

    Add social media and content manager job

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Commune from "./pages/Commune";
+import Jobs from "./pages/Jobs";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/commune" element={<Commune />} />
+          <Route path="/jobs" element={<Jobs />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
